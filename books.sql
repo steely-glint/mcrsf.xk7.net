d8db2fcbb74ee3df541e90f09b61e34a0dfec7db
Paul Waring <paul@xk7.net>
2024-11-23 16:12:49 +0000

    Books schema

---
+++ books.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+    title TEXT,
+    authors TEXT,
+    pages INTEGER,
+    score NUMERIC,
+    score_type TEXT,
+    date_read TEXT,
+    section TEXT
+);
